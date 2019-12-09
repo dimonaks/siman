364130d1aaa50da7c3f7aed84b3b47ba62b1fd78
antonboev22 <toha__22@mail.ru>
2019-12-09 13:35:59 +0300

    1

--- siman/functions.py
+++ siman/functions.py
@@ -4,8 +4,6 @@ import os, tempfile, copy, math, itertools, sys
 import numpy as np
 from operator import itemgetter
 from itertools import product
-import csv
-
 
 try:
     import scipy
@@ -977,6 +975,3 @@ def check_output(filename, check_string, load):
         state = '5. no OUTCAR'
 
     return state
-
-
-
