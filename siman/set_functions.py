e09ad6cbc38d8c06f4610549756e119f9ae726bb
Dmitry Aksenov <dimonaks@gmail.com>
2020-05-01 22:00:02 +0300

    auto

--- siman/set_functions.py
+++ siman/set_functions.py
@@ -760,7 +760,7 @@ def inherit_iset(ise_new, ise_from, varset, override = False, newblockfolder = N
     ise_from = ise_from.strip()
 
     if ise_from not in varset:
-        print_and_log( "\nError! Set "+ise_from+" does not exist. I return new empty set\n")
+        printlog( "\nWarning! Set "+ise_from+" does not exist. I return new empty set\n")
         return InputSet(ise_new)
 
     old = varset[ise_from]
