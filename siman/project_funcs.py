821f3f8dce979ab5136e7b62585dc454f716206f
antonboe22 <toha__22@mail.ru>
2021-04-03 13:45:10 +0300

    1

--- siman/project_funcs.py
+++ siman/project_funcs.py
@@ -4182,7 +4182,7 @@ def optimize(st, name = None, add = 0, ise = '4uis', it_folder = None, fit = 0, 
         del add_loop_dic['calc_method']
     it_new = name
     if add: 
-        add_loop(it_new, ise, 1, up = 'up2', calc_method = calc_method, inherit_option = 'inherit_xred', scale_region = (-5, 3), input_st = st, it_folder = it_folder, **add_loop_dic)
+        add_loop(it_new, ise, 1, up = 'up2', calc_method = calc_method, inherit_option = 'inherit_xred', input_st = st, it_folder = it_folder, **add_loop_dic)
 
     else:
         idd = (it_new+suf, ise, 100)
