e4de922526ef239a69b5ab35e5cfb7a89a119f77
DarthFeiwante <firewitcher@mail.ru>
2021-07-17 16:49:56 +0300

    Fix inherit_c_a

--- siman/structure_functions.py
+++ siman/structure_functions.py
@@ -36,29 +36,44 @@ def inherit_icalc_isotropic(new_structure = '', start_new_version = None,  base_
         version += 1
 
 def inherit_icalc_c_a(new_structure = '', start_new_version = None,  base_calculation = (None, None, None), database = None, min_mult_a = 1, max_mult_a = 1, num_points_a = 2, min_mult_c = 1, max_mult_c = 1,num_points_c = 2, geo_folder=''):
-    from calc_manage import inherit_icalc
-    # Lattice parameter a
-    min_mult_a = min_mult_a
-    max_mult_a = max_mult_a
-    num_points_a = num_points_a
-    step_a = (max_mult_a - min_mult_a)/(num_points_a - 1)
-    mult_list_a = [min_mult_a+step_a*i for i in range(num_points_a)]
+    from classes import inherit_icalc
+    
+    if num_points_a > 1:    
+        # Lattice parameter a
+        min_mult_a = min_mult_a
+        max_mult_a = max_mult_a
+        num_points_a = num_points_a
+        step_a = (max_mult_a - min_mult_a)/(num_points_a - 1)
+        mult_list_a = [min_mult_a+step_a*i for i in range(num_points_a)]
 
-    # Lattice parameter c
-    min_mult_c = min_mult_c
-    max_mult_c = max_mult_c
-    num_points_c = num_points_c
-    step_c = (max_mult_c - min_mult_c)/(num_points_c - 1)
-    mult_list_c = [min_mult_c+step_c*i for i in range(num_points_c)]
+    if num_points_c > 1:  
+        # Lattice parameter c
+        min_mult_c = min_mult_c
+        max_mult_c = max_mult_c
+        num_points_c = num_points_c
+        step_c = (max_mult_c - min_mult_c)/(num_points_c - 1)
+        mult_list_c = [min_mult_c+step_c*i for i in range(num_points_c)]
 
     print('database', database)
 
     version = start_new_version
-    for j in mult_list_a:
-        for k in mult_list_c:
-            inherit_icalc('c_a',   new_structure,  version, base_calculation, database, mult_a = j, mult_c = k, geo_folder=geo_folder)
+
+    if num_points_a > 1 and num_points_c > 1:    
+        for j in mult_list_a:
+            for k in mult_list_c:
+                inherit_icalc('c_a',   new_structure,  version, base_calculation, database, mult_a = j, mult_c = k, geo_folder=geo_folder)
+                version += 1
+
+    elif num_points_c = 1:
+        for j in mult_list_a:
+            inherit_icalc('c_a',   new_structure,  version, base_calculation, database, mult_a = j, mult_c = 1, geo_folder=geo_folder, override=override)
             version += 1
 
+    elif num_points_a = 1:
+        for j in mult_list_c:
+            inherit_icalc('c_a',   new_structure,  version, base_calculation, database, mult_a = 1, mult_c = j, geo_folder=geo_folder, override=override)
+            version += 1
+            
 def inherit_icalc_x_y(new_structure = '', start_new_version = None,  base_calculation = (None, None, None), database = None, min_mult_a = 1, max_mult_a = 1, num_points_a = 2, min_mult_b = 1, max_mult_b = 1,num_points_b = 2, geo_folder=''):
     from calc_manage import inherit_icalc
     
