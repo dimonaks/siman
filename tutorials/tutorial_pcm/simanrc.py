7b627a1b93116d510cb61a323ccfd2ee554a8834
antonboev22 <toha__22@mail.ru>
2024-05-14 11:22:55 +0300

    tutorial PCM

--- tutorials/tutorial_pcm/simanrc.py
+++ tutorials/tutorial_pcm/simanrc.py
@@ -2,20 +2,20 @@
 User-related settings for siman
 """
 
-local_path = '/home/anton/jupyter/siman/'
+local_path = '/home/your_local_path/'
 PATH2POTENTIALS = local_path+'potcars'
 PATH2JMOL = 'java -jar Jmol.jar'
 AUTO_UPDATE_DB = True
-pmgkey = "AWqKPyV8EmTRlf1t" #API_KEY can be generated in the following webpage: https://materialsproject.org/dashboard 
+pmgkey = "your_pmg_key" #API_KEY can be generated in the following webpage: https://materialsproject.org/dashboard 
 
 
 """Cluster settings"""
-DEFAULT_CLUSTER = 'geo' #short name of cluster
-user = 'a.boev'
+DEFAULT_CLUSTER = 'cee' #short name of cluster
+user = 'username'
 
 from siman.header import CLUSTERS
 
-CLUSTERS['geo'] = {'address':user+'@95.71.121.195', #cluster address
+CLUSTERS['geo'] = {'address':user+'@ip_address', #cluster address
 'vasp_com':'mpirun /opt/vasp/bin/vasp_std', #command for VASP perfoming on cluster
 'homepath':'/home/'+user, #path to home directory on cluster
 'schedule':'SLURM', #type of schedule system using on cluster
@@ -23,23 +23,10 @@ CLUSTERS['geo'] = {'address':user+'@95.71.121.195', #cluster address
 'corenum':2, #number of cores for perfoming of one job on cluster
 }
 
-CLUSTERS['raz'] = {'address':user+'@10.16.77.19', #cluster address
-'vasp_com':'mpirun vasp_std', #command for VASP perfoming on cluster
-'homepath':'/home/'+user, #path to home directory on cluster
-'schedule':'SLURM', #type of schedule system using on cluster
-'walltime':'2:00:00', #maximum time for job execution, hours:minutes:seconds, after this time since job was started process will be killed by system
-'corenum':2, #number of cores for perfoming of one job on cluster
-'modules':'module load devtools/compiler/nvhpc/20.11; \
-module load q-ch/vasp/5.4.4_OPT; \
-\nulimit -s unlimited\n\
-'
-}
-
-
 
 CLUSTERS['cee'] = {'vasp_com':'mpirun vasp_std',
-'address':'sk',
-'homepath':'/home/a.boev/',
+'address':'ip_address',
+'homepath':'/home/'+user,
 'schedule':'SLURM',
 'walltime':'24:00:00',
 'corenum':16,
@@ -47,9 +34,9 @@ CLUSTERS['cee'] = {'vasp_com':'mpirun vasp_std',
 'partition':'AMG-medium',
 }
 
-CLUSTERS['cee-omc'] = {'vasp_com':'mpirun vasp_std',
-'address':'sk',
-'homepath':'/home/a.boev/',
+CLUSTERS['cee-omc'] = {'vasp_com':'mpirun vasp_std',  #VASP compiled with OMC: Q-Ch/VASP/5.4.4_OMC
+'address':'ip_address',
+'homepath':'/home/'+user,
 'schedule':'SLURM',
 'walltime':'72:00:00',
 'corenum':16,
@@ -58,9 +45,9 @@ CLUSTERS['cee-omc'] = {'vasp_com':'mpirun vasp_std',
 }
 
 
-CLUSTERS['cee-pcm'] = {'vasp_com':'mpirun vasp_std',
-'address':'sk',
-'homepath':'/home/a.boev/',
+CLUSTERS['cee-pcm'] = {'vasp_com':'mpirun vasp_std', #VASP compiled with VASPsol: Q-Ch/VASP/5.4.4_SOL
+'address':'ip_address',
+'homepath':'/home/'+user,
 'schedule':'SLURM',
 'walltime':'72:00:00',
 'corenum':16,
