4482ef733702a34f6ae3f48bea62a904559b0648
dimonaks <dimonaks@gmail.com>
2026-05-08 18:04:12 +0300

    u-ramping batch bug

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
-rm dist/*
-python setup.py sdist 
+#!/usr/bin/env bash
+set -e
+
+rm -rf build dist siman.egg-info
+
+python -m pip install --upgrade pip setuptools wheel build twine
+
+python -m build
+
+python -m twine check dist/*
+
 python -m twine upload --repository siman dist/*
